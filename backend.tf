4c7df8dd9b0302dd6e8a1c9ed08972d4df3cb57f
AngelWitk <angel.witk@gmail.com>
2023-12-11 22:24:23 +0100

    :fix

--- backend.tf
+++ backend.tf
@@ -2,12 +2,16 @@ terraform {
   required_version = ">= 1.0.11"
   backend "gcs" {
     bucket = "t8-terraform-state-file"
-    prefix = "dev" }
-
+    prefix = "dev"
+  }
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "4.41.0" }
+      version = "4.41.0"
+    }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "4.41.0" } } }
+      version = "4.41.0"
+    }
+  }
+}
