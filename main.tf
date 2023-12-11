da3d6e5571cb51acd9bb39bf580310411f9f28d4
AngelWitk <angel.witk@gmail.com>
2023-12-11 22:50:20 +0100

    :fix

--- main.tf
+++ main.tf
@@ -28,10 +28,8 @@ resource "google_storage_bucket" "t8-static-site" {
 
 resource "google_storage_bucket_object" "t8-static-site" {
   bucket = google_storage_bucket.t8-static-site.name
-  role   = "roles/storage.objectViewer"
-  members = [
-    "allUsers"
-  ]
+  role   = "READER"
+  entity = "allUsers"
 }
 
 resource "google_storage_bucket_object" "picture" {
